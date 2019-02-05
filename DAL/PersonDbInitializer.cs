4a31ad605bf55c31c7c70f41fe6b87e33c2e4dfe
Ben Jen <benjendrzejak@quickenloans.com>
2019-02-04 19:06:38 -0500

    Fixed initializer bug

--- DAL/PersonDbInitializer.cs
+++ DAL/PersonDbInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using Lab21.Models;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 
 namespace Lab21.DAL
 {
@@ -12,7 +13,7 @@ namespace Lab21.DAL
                 new Items {Name = "Dark Roast", Description = "Spicy bean juice", Quantity = 1, Price = 5.00},
             };
 
-            items.ForEach(s => context.Items.Add(s));
+            items.ForEach(s => context.Items.AddOrUpdate(s));
 
             context.SaveChanges();
 
